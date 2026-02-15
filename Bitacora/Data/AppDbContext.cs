0e9375a2d6240cad3166097e413f146924611485
codeCraftsman08 <segura.badilla@gmail.com>
2026-02-15 11:38:52 -0600

    Agrego carpeta Bitacora al repositorio Progra-5

---
+++ Bitacora/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using LogService.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace LogService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Bitacora> Bitacoras { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configuración adicional si fuera necesaria
+            modelBuilder.Entity<Bitacora>().ToTable("Bitacoras");
+        }
+    }
+}
