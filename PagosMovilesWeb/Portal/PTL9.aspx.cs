379a831ce4f1377b719c3bfaec1b1fcca8ef240c
dvid <DAVIDCARRANZAG07@GMAIL.COM>
2026-03-17 20:52:58 -0600

    Update branch local

--- PagosMovilesWeb/Portal/PTL9.aspx.cs
+++ PagosMovilesWeb/Portal/PTL9.aspx.cs
@@ -20,9 +20,18 @@ namespace PagosMovilesWeb.Portal
 
             if (!IsPostBack)
             {
+                // 🔥 FAKE LOGIN (solo para pruebas)
+                Session["AccessToken"] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Imp1YW4ucGVyZXpAbWFpbC5jb20iLCJpZCI6IjEwMTAwMDAwMSIsIm5iZiI6MTc3MzcyNTU3MiwiZXhwIjoxNzczNzI1ODcyLCJpYXQiOjE3NzM3MjU1NzIsImlzcyI6IlR1QXBwQXV0aCIsImF1ZCI6IlR1QXBwVXN1YXJpb3MifQ.RFfgCHlbgNiMCI4cNGAn1SbhRq15DG4ZMOiNoIZtad4";
+                Session["NombreCompleto"] = "David Prueba";
+
+                txtNombreOrigen.Text = Session["NombreCompleto"].ToString();
+            }
+
+            /*if (!IsPostBack)
+            {
                 txtNombreOrigen.Text = SessionHelper.NombreCompleto;
                 pnlMensaje.Visible = false;
-            }
+            }*/
         }
 
         protected void btnTransferir_Click(object sender, EventArgs e)
