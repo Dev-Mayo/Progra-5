35b3dac975842e6e06de3567357f320cb568c87a
Dev-Mayo <difueba@gmail.com>
2026-03-18 23:56:02 -0600

    Delete Tarea Corta 2 directory

--- Tarea Corta 2/TareaCorta2/Config/MongoSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TareaCorta2.Config
-{
-    public class MongoSettings
-    {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-        public string ClientesCollection { get; set; }
-    }
-}
