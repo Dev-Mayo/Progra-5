c8c81861a3fe9806b24a323a2d4d589de59aa469
Dev-Mayo <difueba@gmail.com>
2026-03-21 13:11:38 -0600

    Delete Tarea Corta 2 directory

--- Tarea Corta 2/Gateways/ApiGateway/Models/ResumenOrden.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApiGateway.Models
-{
-    public class ResumenOrden
-    {
-        public string OrdenId { get; set; }
-        public int Cantidad { get; set; }
-        public dynamic ProductoDetalle { get; set; }
-    }
-}
